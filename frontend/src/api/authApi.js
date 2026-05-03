344838aa65b6c4e3bf5dc7a5bcee132481f99a7a
Navin <krnaveenraj809@gmail.com>
2026-05-03 17:55:28 +0530

    updated port in authapi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -11,12 +11,6 @@
 
 import API from "./axios";
 
-// Updated port to 32000 to match the working backend in image_0d9fb9.png
-export const loginUser = (data) =>
-  API.post("http://localhost:32000/auth/login", data);
-
-export const registerUser = (data) =>
-  API.post("http://localhost:32000/auth/register", data);
-
-export const validateToken = () =>
-  API.get("http://localhost:32000/auth/validate");
+export const loginUser = (data) => API.post("/login", data);
+export const registerUser = (data) => API.post("/register", data);
+export const validateToken = () => API.get("/validate");
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,27 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost", // we'll override per service
+// });
+
+// // Attach token automatically
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost", // we'll override per service
+  // Port updated to 32000 to match image_0d9fb9.png
+  baseURL: "http://localhost:32000/auth", 
 });
 
-// Attach token automatically
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
