1201dfcbc40bf0f125f11a95ce5fbe430abdbab0
Navin <krnaveenraj809@gmail.com>
2026-05-03 21:57:32 +0530

    image version update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,23 +11,34 @@
 // import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'; // This must be present
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
-    // 1. Allow Vite to accept connections from outside the container
     host: '0.0.0.0',
     port: 80,
-    
-    // 2. Critical for WSL2: Enable polling to detect file changes correctly
-    watch: {
-      usePolling: true,
-    },
-    
-    // 3. Match the HMR port to your Kubernetes NodePort (30007)
-    // This prevents the "Pending" websocket connection seen in image_5c3bc1.png
-    hmr: {
-      clientPort: 30007,
-    },
   },
-})
+});
+
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     // 1. Allow Vite to accept connections from outside the container
+//     host: '0.0.0.0',
+//     port: 80,
+    
+//     // 2. Critical for WSL2: Enable polling to detect file changes correctly
+//     watch: {
+//       usePolling: true,
+//     },
+    
+//     // 3. Match the HMR port to your Kubernetes NodePort (30007)
+//     // This prevents the "Pending" websocket connection seen in image_5c3bc1.png
+//     hmr: {
+//       clientPort: 30007,
+//     },
+//   },
+// })
