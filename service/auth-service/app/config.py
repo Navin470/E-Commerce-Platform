702f1d6a6d7d5218854aebed7348dfb4333c8a70
Navin <krnaveenraj809@gmail.com>
2026-05-03 11:24:03 +0530

    created config for frontend

--- service/auth-service/app/config.py
+++ service/auth-service/app/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    FRONTEND_URL = os.getenv("http://192.168.127.2:30007", "*") # '*' allows all for debugging
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET", "super-secret-key")
